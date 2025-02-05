80dfc51703f746180ab0b5e752530189aaecb19c
Karan <Karansj4723@gmail.com>
2025-02-05 08:46:14 +0530

    Added binary search

---
+++ linearsearch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int linearSearch(int arr[], int n, int key){
+    for(int i = 0; i < n; i++){
+        
+        if(arr[i] == key){
+            return i;
+        }
+    }
+    return -1; // element not found
+}
+
+int main(){
+    int arr[] = {10, 20, 30, 40, 50};
+
+    int key = 40;
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = linearSearch(arr, n, key);
+
+    if(result != -1){
+        printf("Element fount at %d\n", result);
+    }
+    else{
+        printf("Element not found");
+    }
+}
